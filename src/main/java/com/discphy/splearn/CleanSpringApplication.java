6548fb2780805fdf061c12b6b9c0111d98be36cc
discphy <dinophy@nate.com>
2025-09-29 13:04:29 +0900

    Initial commit

---
+++ src/main/java/com/discphy/splearn/CleanSpringApplication.java
@@ -0,0 +1,13 @@
+package com.discphy.splearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/discphy/splearn/CleanSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discphy.splearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
